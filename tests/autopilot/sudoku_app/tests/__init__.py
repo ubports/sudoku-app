eee333a2098c372075d4a49c78ecc867cc0f79d1
nskaggs <nicholas.skaggs@canonical.com>
2013-07-11 12:46:16 -0400

    base for video tutorial example, adding ubuntusdk emulator

--- tests/autopilot/sudoku_app/tests/__init__.py
+++ tests/autopilot/sudoku_app/tests/__init__.py
@@ -4,3 +4,57 @@
 # This program is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License version 3, as published
 # by the Free Software Foundation.
+
+"""Sudoku app autopilot tests."""
+
+import os.path
+
+from autopilot.input import Mouse, Touch, Pointer
+from autopilot.platform import model
+from autopilot.testcase import AutopilotTestCase
+
+from sudoku_app.emulators.main_window import MainWindow
+from sudoku_app.emulators.ubuntusdk import ubuntusdk
+
+
+class SudokuTestCase(AutopilotTestCase):
+
+    """A common test case class that provides several useful methods for
+    sudoku-app tests.
+
+    """
+    if model() == 'Desktop':
+        scenarios = [('with mouse', dict(input_device_class=Mouse))]
+    else:
+        scenarios = [('with touch', dict(input_device_class=Touch))]
+
+    local_location = "../../sudoku-app.qml"
+
+    def setUp(self):
+        self.pointing_device = Pointer(self.input_device_class.create())
+        super(SudokuTestCase, self).setUp()
+        if os.path.exists(self.local_location):
+            self.launch_test_local()
+        else:
+            self.launch_test_installed()
+
+    def launch_test_local(self):
+        self.app = self.launch_test_application(
+            "qmlscene",
+            self.local_location,
+            app_type='qt')
+
+    def launch_test_installed(self):
+        self.app = self.launch_test_application(
+            "qmlscene",
+            "/usr/share/sudoku-app/sudoku-app.qml",
+            "--desktop_file_hint=/usr/share/applications/sudoku-app.desktop",
+            app_type='qt')
+
+    @property
+    def main_window(self):
+        return MainWindow(self.app)
+
+    @property
+    def ubuntusdk(self):
+        return ubuntusdk(self, self.app)
--- tests/autopilot/sudoku_app/tests/test_sudoku.py
+++ tests/autopilot/sudoku_app/tests/test_sudoku.py
@@ -24,15 +24,3 @@ class TestMainWindow(SudokuTestCase):
 
     def tearDown(self):
         super(TestMainWindow, self).tearDown()
-
-    def test_toolbar_shows(self):
-        """Make sure that dragging from the bottom reveals the hidden
-        toolbar."""
-        toolbar = self.main_window.get_toolbar()
-
-        x, y, w, h = toolbar.globalRect
-        tx = x + (w / 2)
-        ty = y + (h - 2)
-
-        self.pointing_device.drag(tx, ty, tx, ty - h)
-        self.assertThat(toolbar.state, Eventually(Equals("spread")))
